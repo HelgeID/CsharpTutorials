78283eb03621308d1e7008d787facd542b149a65
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:46:04 +0200

    Add files via upload

---
+++ list1.0.4, класи (доступ до закритих полів (private)).cs
@@ -0,0 +1,25 @@
+﻿//класи, доступ до закритих полів (private)
+
+    class MyClass
+    {
+        private string myField = null;
+
+        //Метод-мутатор
+        public void SetField(string value)
+        {
+            myField = value;
+        }
+
+        //Метод-аксесор
+        public string GetField()
+        {
+            return myField;
+        }
+    }
+
+    ...
+    MyClass ins = new MyClass();
+    ins.SetField("Hello, World!");
+    string @string = ins.GetField();
+    Console.WriteLine(@string);//Hello, World!
+    
