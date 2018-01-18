e46926e39cbf4b89741a1e50e9ab130623c0e4a2
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:48:49 +0200

    Add files via upload

---
+++ list1.1.0, класи (техніка фабричних методів).cs
@@ -0,0 +1,50 @@
+﻿//класи (техніка фабричних методів)
+
+    class MyClass
+    {
+        //конструктор
+        public MyClass()
+        {
+            Console.WriteLine("MyClass created!");
+        }
+    }
+
+    class Factory
+    {
+        //даний метод є фабричним, бо вертає значення породженого продукта (MyClass)
+        public MyClass FactoryMethod()
+        {
+            return new MyClass();
+        }
+    }
+
+...
+Factory factory = new Factory();
+MyClass my = factory.FactoryMethod();
+
+
+
+//не фабричні методи
+
+    class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("MyClass created!");
+        }
+    }
+
+    class Second
+    {
+        private MyClass obj = null;
+
+        //даний метод не є фабричним
+        public void Method()
+        {
+            obj = new MyClass();
+        }
+    }
+
+...
+Second my = new Second();
+my.Method();
