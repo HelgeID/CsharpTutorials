df40097baee3390e7342052f3659893275d75855
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:50:25 +0200

    Add files via upload

---
+++ list1.1.2, класи (інкапсуляція всередині класа не працює).cs
@@ -0,0 +1,22 @@
+﻿//класи (інкапсуляція всередині класа не працює)
+
+    class MyClass
+    {
+        MyClass my = null;
+
+        private void MyMethod()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void CallMyMethod()
+        {
+            my = new MyClass();
+            my.MyMethod();//доступно
+        }
+    }
+
+    ...
+    MyClass my = new MyClass();
+    my.CallMyMethod();
+    //my.MyMethod();//недоступно
