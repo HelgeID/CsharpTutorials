1792240b6501e9e3fd986db99afa4c2d28034ef4
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:49:06 +0200

    Add files via upload

---
+++ list1.1.1, класи (дві силки, які вказують на один і той же об'єкт).cs
@@ -0,0 +1,23 @@
+﻿//класи, дві силки, які вказують на один і той же об'єкт
+
+    public class MyClass
+    {
+        public int myNumber;
+
+        public int takeNumber()
+        {
+            return myNumber;
+        }
+    }
+
+...
+MyClass obj1 = new MyClass();
+MyClass obj2 = obj1;
+
+obj1.myNumber = 100;
+Console.WriteLine(obj1.takeNumber());//100
+Console.WriteLine(obj2.takeNumber());//100
+
+obj2.myNumber = 200;
+Console.WriteLine(obj1.takeNumber());//200
+Console.WriteLine(obj2.takeNumber());//200
