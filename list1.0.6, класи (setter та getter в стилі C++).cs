560f5a459eea07e832f4fddf1bdf591cd15e86d5
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:46:43 +0200

    Add files via upload

---
+++ list1.0.6, класи (setter та getter в стилі C++).cs
@@ -0,0 +1,24 @@
+﻿//класи (setter та getter в стилі C++)
+
+    class MyClass
+    {
+        string str = string.Empty;//private
+
+        //метод доступу set
+        public void set(string _str)
+        {
+            str = _str;
+        }
+
+        //метод доступу get
+        public string get()
+        {
+            return str;
+        }
+    }
+
+...
+MyClass obj = new MyClass();
+obj.set("Hello, World!");
+string str = obj.get();
+Console.WriteLine(str);//Hello, World!
