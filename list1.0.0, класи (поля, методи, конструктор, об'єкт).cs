1a2778bb118e1e853b00b9a05766b7c574802285
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:43:21 +0200

    Add files via upload

---
+++ list1.0.0, класи (поля, методи, конструктор, об'єкт).cs
@@ -0,0 +1,22 @@
+﻿//класи (поля, методи, конструктор, об'єкт)
+
+    class MyClass
+    {
+        //поле
+        public string myField;
+
+        //конструктор
+        public MyClass()
+        {
+        }
+
+        //метод(функція)
+        public void MyMethod()
+        {
+        }
+    }
+
+    ...
+    MyClass obj = new MyClass();//екземпляр класа(об'єкт)
+    obj.MyMethod();//виклик метода
+    
