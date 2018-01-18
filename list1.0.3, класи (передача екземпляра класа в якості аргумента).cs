2107718ab0a56b2ce856fccff19e0129e1147c23
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:45:46 +0200

    Add files via upload

---
+++ list1.0.3, класи (передача екземпляра класа в якості аргумента).cs
@@ -0,0 +1,24 @@
+﻿//класи, передача екземпляра класа в якості аргумента
+
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Call Method");
+        }
+    }
+
+    class MyClass2
+    {
+        public void CallMethod(MyClass my)
+        {
+            my.Method();
+        }
+    }
+
+    ...
+    MyClass my = new MyClass();
+    MyClass2 my2 = new MyClass2();
+
+    my2.CallMethod(my);
+    
