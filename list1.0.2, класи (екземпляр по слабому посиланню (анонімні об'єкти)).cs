425e4282c4e2fd36c94f03cf6c6f1c1b29b4f8a7
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-18 11:44:34 +0200

    Add files via upload

---
+++ list1.0.2, класи (екземпляр по слабому посиланню (анонімні об'єкти)).cs
@@ -0,0 +1,13 @@
+﻿//класи, екземпляр по слабому посиланню (анонімні об'єкти)
+
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    ...
+    new MyClass().Method();
+    
