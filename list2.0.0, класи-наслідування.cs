d1751e38472ee81e3a547ad42d59fe72fd2a0a5e
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-22 12:00:20 +0200

    Add files via upload

---
+++ list2.0.0, класи-наслідування.cs
@@ -0,0 +1,43 @@
+﻿//класи-наслідування
+
+//приклад1
+    //створення класа Base
+    class Base
+    {
+        public string BaseField;
+        public void BaseMethod()
+        {
+        }
+    }
+
+    //створення класа Derived, та наслідування від Base
+    class Derived : Base
+    {
+        public void DerivedMethod()
+        {
+        }
+    }
+
+...
+Derived obj = new Derived();
+obj.BaseField = "Hello, World!";
+obj.BaseMethod();
+obj.DerivedMethod();
+
+
+
+//приклад2
+    class Base
+    {
+        public string Str { get; set; }
+    }
+
+    class Derived : Base
+    {
+    }
+
+...
+Base obj = new Base { Str = "uses Base class" };
+Console.WriteLine(obj.Str);//uses Base class
+obj = new Derived { Str = "uses Derived class" };
+Console.WriteLine(obj.Str);//uses Derived class
