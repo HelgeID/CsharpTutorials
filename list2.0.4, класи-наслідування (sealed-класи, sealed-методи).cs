e47141c8c0a60571e3eb80e952b7e0c0048b7223
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-22 12:05:30 +0200

    Add files via upload

---
+++ list2.0.4, класи-наслідування (sealed-класи, sealed-методи).cs
@@ -0,0 +1,39 @@
+﻿//класи-наслідування (sealed-класи, sealed-методи)
+
+//заборона наслідування класа (запечатаний клас)
+    sealed class MyClass
+    {
+        public int x;
+        public int y;
+    }
+
+    //error
+    //class Derived : MyClass
+    //{
+    //}
+
+...
+MyClass obj = new MyClass();
+obj.x = 100;
+obj.y = 200;
+
+
+
+//заборона переоприділення метода (запечатаний метод)
+    class ClassA
+    {
+        public virtual void MyMethod1() { }
+        public virtual void MyMethod2() { }
+    }
+
+    class ClassB : ClassA
+    {
+        sealed public override void MyMethod1() { } //sealed, як заборона переоприділення метода
+        public override void MyMethod2() { }
+    }
+
+    class ClassC : ClassB
+    {
+        //public override void MyMethod1() {} //error: спроба переоприділення метода (метод запечатаний)
+        public override void MyMethod2() { }
+    }
