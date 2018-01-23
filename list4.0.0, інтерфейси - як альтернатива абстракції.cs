80166f0d94265365fc44c4eb6f131c2515c06279
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-23 14:03:49 +0200

    Add files via upload

---
+++ list4.0.0, інтерфейси - як альтернатива абстракції.cs
@@ -0,0 +1,18 @@
+﻿//інтерфейси - як альтернатива абстракції
+
+    interface IInterface
+    {
+        void Show();//метод по-замовчуванню відкритий (немає смисла робити закритим)
+    }
+
+    class MyClass : IInterface
+    {
+        public void Show()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+...
+MyClass obj = new MyClass();
+obj.Show();//Hello, World!
