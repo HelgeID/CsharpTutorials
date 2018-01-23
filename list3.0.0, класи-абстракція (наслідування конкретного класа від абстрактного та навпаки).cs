063af94e04d95da30497229d0110beb9b931f748
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-23 13:52:51 +0200

    Add files via upload

---
+++ list3.0.0, класи-абстракція (наслідування конкретного класа від абстрактного та навпаки).cs
@@ -0,0 +1,55 @@
+﻿//класи-абстракція (наслідування конкретного класа від абстрактного та навпаки)
+
+//приклад1 (конкретний від абстрактного)
+
+    //абстрактний клас
+    abstract class Abstract
+    {
+        public abstract void MyMethod();//те ж саме, що: public virtual void MyMethod() {}
+    }
+
+    //конкретний клас
+    class Concrete : Abstract
+    {
+        //переопреділення метода MyMethod()
+        public override void MyMethod()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+...
+Abstract obj = new Concrete();
+obj.MyMethod();//Hello, World!
+
+
+
+//приклад2 (абстрактний від конкретного)
+
+    //конкретний клас
+    class Concrete
+    {
+        public void Operation()
+        {
+        }
+    }
+
+    //абстрактний клас
+    abstract class Abstract : Concrete
+    {
+        public abstract void MyMethod();
+    }
+
+    //мій клас
+    class MyClass : Abstract
+    {
+        public override void MyMethod()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+...
+Abstract obj = new MyClass();
+obj.Operation();
+obj.MyMethod();
