7c7ef17e1999762df215deaf092e69b3d426ec46
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-23 14:01:26 +0200

    Add files via upload

---
+++ list3.0.3, класи-абстракція (реалізація абстрактного метода в похідному класі).cs
@@ -0,0 +1,47 @@
+﻿//класи-абстракція (реалізація абстрактного метода в похідному класі)
+
+    abstract class Abstract
+    {
+        public Abstract()
+        {
+            Console.WriteLine("1 Abstract class");
+
+            this.AbstractMethod();//Викликається реалізація методу з похідного класу
+
+            Console.WriteLine("2 Abstract class");
+        }
+
+        public abstract void AbstractMethod();//оголошення
+    }
+
+    class Concrete : Abstract
+    {
+        string str = "value 1";
+
+        public Concrete()
+        {
+            Console.WriteLine("3 Concrete class");
+            str = "value 2";
+        }
+
+        //реалізація
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("{0}", str);
+        }
+    }
+
+...
+Abstract obj = new Concrete();
+Console.WriteLine(new string('-', 10));
+obj.AbstractMethod();
+
+/*
+1 Abstract class
+value 1
+2 Abstract class
+3 Concrete class
+----------
+value 2
+
+ */
