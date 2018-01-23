cdb38581f3fdd65137af6d07c683b037ea8f29c9
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-23 14:00:46 +0200

    Add files via upload

---
+++ list3.0.2, класи-абстракція (наслідування від абстрактного класа та реалізація їх методів).cs
@@ -0,0 +1,36 @@
+﻿//класи-абстракція (наслідування від абстрактного класа та реалізація їх методів)
+
+    //абстрактний клас A
+    abstract class AbstractA
+    {
+        public abstract void OperationA();
+    }
+
+    //абстрактний клас B
+    abstract class AbstractB : AbstractA
+    {
+        public abstract void OperationB();
+    }
+
+    //конкретний клас
+    class Concrete : AbstractB
+    {
+        //реалізація методів обов'язкова
+        public override void OperationA()
+        {
+            Console.WriteLine("OperationA");
+        }
+
+        public override void OperationB()
+        {
+            Console.WriteLine("OperationB");
+        }
+    }
+
+...
+AbstractA objA = new Concrete();
+objA.OperationA();
+
+AbstractB objB = new Concrete();
+objB.OperationA();
+objB.OperationB();
