a147a9b4db753c20a013e91baff7cafb46ed5def
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-24 13:38:12 +0200

    Add files via upload

---
+++ list5.0.1, індексатори (двохвимірний масив).cs
@@ -0,0 +1,18 @@
+﻿//індексатори (двохвимірний масив)
+    class MyClass
+    {
+        private int[,] array = new int[3, 3];
+
+        public int this[int index1, int index2]
+        {
+            get { return array[index1, index2]; }
+            set { array[index1, index2] = value; }
+        }
+    }
+
+...
+MyClass my = new MyClass();
+my[1, 1] = 20;
+
+Console.WriteLine(my[1, 1]);//20
+Console.WriteLine(my[0, 0]);//0
