9a4fcf6590bb43988547e8f0155398993f40f221
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-25 18:50:05 +0200

    Add files via upload

---
+++ list1.1.3, константні поля (константи).cs
@@ -0,0 +1,15 @@
+﻿//константні поля (константи)
+    class MyClass
+    {
+        //константи не можуть бути статичними
+        public const double PI = 3.14;
+
+        public MyClass()
+        {
+        }
+    }
+
+...
+MyClass obj = new MyClass();
+
+Console.WriteLine(MyClass.PI);
