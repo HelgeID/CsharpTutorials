93869f1662dbbb3a42ff0786f1b3322620d9cf34
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-25 18:50:33 +0200

    Add files via upload

---
+++ list1.1.4, поля readonly.cs
@@ -0,0 +1,26 @@
+﻿//поля readonly
+    class MyClass
+    {
+        public readonly int rField = 0;
+
+        public MyClass()
+        {
+            rField = 1;//можна змінювати чи ініціалізувати тільки в конструкторі
+        }
+
+        public MyClass(int rField)
+        {
+            this.rField = rField;//можна змінювати чи ініціалізувати тільки в конструкторі
+        }
+
+        public void Change(int rField)
+        {
+            //this.rField = rField;//error
+        }
+    }
+
+...
+MyClass obj1 = new MyClass();
+Console.WriteLine(obj1.rField);//1
+MyClass obj2 = new MyClass(10);
+Console.WriteLine(obj2.rField);//10
