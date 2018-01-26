3f1a4324acc96cfeea174dc4f49b054f98f14ee5
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-26 09:18:08 +0200

    Add files via upload

---
+++ list6.0.6, заміщення статичних методів.cs
@@ -0,0 +1,35 @@
+﻿//заміщення статичних методів
+
+//      1. Статичні методи можна заміщати.
+//      2. Cтатичні методи не можуть бути віртуальними, абстрактними, переоприділеними.
+
+    abstract class Base
+    {
+        //error - StaticMethod не може бути поміченим, як virtual
+        //public static virtual void StaticMethod()
+        //{
+        //}
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Base Static Method");
+        }
+    }
+
+    class Derived: Base
+    {
+        //error - StaticMethod не може бути поміченим, як override
+        //public static override void StaticMethod()
+        //{
+        //}
+
+        //new - робиться заміщення
+        public static new void StaticMethod()
+        {
+            Console.WriteLine("Derived Static Method");
+        }
+    }
+
+...
+Base.StaticMethod();//Base Static Method
+Derived.StaticMethod();//Derived Static Method
