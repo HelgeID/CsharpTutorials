702fcd14eea2313629ad0d48974f7a261a6c6fd3
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-26 09:16:33 +0200

    Add files via upload

---
+++ list6.0.3, статичний конструктор.cs
@@ -0,0 +1,63 @@
+﻿//статичний конструктор
+
+//приклад1
+    class MyClass
+    {
+        static MyClass()
+        {
+            Console.WriteLine("launch of constructor");
+        }
+    }
+
+...
+MyClass obj1 = new MyClass();//тут спрацьовує статичний конструктор
+MyClass obj2 = new MyClass();
+
+
+
+//приклад2
+    static class MyClass
+    {
+        private static int number;
+
+        public static int Number
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+    }
+
+...
+//MyClass obj = new MyClass();//error
+MyClass.Number = 100;
+Console.WriteLine(MyClass.Number);//100
+
+
+
+//приклад3
+    static class MyClass
+    {
+        //статичне поле readonly повинно бути проініціалізоване
+        static readonly int rMyField = 10;
+
+        //статична властивість тільки для читання
+        public static int RMyField
+        {
+            get { return MyClass.rMyField; }
+        }
+
+        //статичний конструктор - спрацьовує автоматично
+        static MyClass()
+        {
+            rMyField = 100;
+        }
+    }
+
+...
+Console.WriteLine(MyClass.RMyField);//100
