55842c4b9bd9aea19c1071d71fb4d32d7912d9b6
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-26 09:18:53 +0200

    Add files via upload

---
+++ list6.0.7, singleton - вказання екзкмплярів на один і той же об'єкт.cs
@@ -0,0 +1,31 @@
+﻿//singleton - вказання екзкмплярів на один і той же об'єкт
+    class Singleton
+    {
+        private static Singleton ins = null;
+
+        // Конструктор - "protected"
+        protected Singleton()
+        {
+        }
+
+        // Фабричний метод
+        public static Singleton Instance()
+        {
+            //якщо об'єкт ще не створений
+            if (ins == null)
+            {
+                //створюємо новий екземпляр
+                ins = new Singleton();
+            }
+
+            //повертаємо силку
+            return ins;
+        }
+    }
+
+...
+Singleton ins1 = Singleton.Instance();
+Singleton ins2 = Singleton.Instance();
+
+if (ins1 == ins2)
+    Console.WriteLine("Links => object");//силки вказують на один і той об'єкт
