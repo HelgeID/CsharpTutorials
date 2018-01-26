c6ae3b58a7beb8c8b0380fcf42615a34c50a4686
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-26 09:16:04 +0200

    Add files via upload

---
+++ list6.0.2, статичний клас.cs
@@ -0,0 +1,28 @@
+﻿//статичний клас
+    static class StaticClass// :object //наслідування від Object(явно та неявно)
+    {
+        //Конструктор не доступний (видає error при реалізації)
+        //public StaticClass()
+        //{
+        //}
+
+        //Статичний конструктор
+        static StaticClass()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        //Статичний метод
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static Method");
+        }
+
+        //Не статичний метод (видає error при реалізації)
+        //public void NoStaticMethod()
+        //{
+        //}
+    }
+
+...
+StaticClass.StaticMethod();
