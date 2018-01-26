fa25352a33b373cbaeb48374337eabf23d91cc98
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-26 09:17:42 +0200

    Add files via upload

---
+++ list6.0.5, статичний метод в абстрактному класі.cs
@@ -0,0 +1,25 @@
+﻿//статичний метод в абстрактному класі
+
+    abstract class Abstract
+    {
+        //статичний фабричний метод
+        public static Abstract CreateObject()
+        {
+            //на куче створює екземпляр класа та приводе його до типу Abstract
+            return new Concrete();
+        }
+
+        public abstract void Method();
+    }
+
+    class Concrete: Abstract
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+...
+Abstract ins = Abstract.CreateObject();
+ins.Method();//Hello, World!
