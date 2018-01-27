67e28c3d1f41ab4d87b2f4d499819e2c3e181f93
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-27 13:32:05 +0200

    Add files via upload

---
+++ list8.0.2, вложені класи (нестатичний Nested-клас у статичному класі та навпаки).cs
@@ -0,0 +1,48 @@
+﻿//нестатичний Nested-клас у статичному класі
+    public static class MyClass
+    {
+        static MyClass()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("Static Method");
+        }
+
+        //статичні класи можуть у собі містити нестатичні Nested-класи
+        public class Nested
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method of Nested Class");
+            }
+        }
+    }
+
+...
+MyClass.Nested ins = new MyClass.Nested();
+ins.Method();//Method Nested Class
+MyClass.StaticMethod();
+/*
+Static Constructor
+Static Method
+*/
+
+
+
+//статичний Nested-клас у нестатичному класі
+    public class MyClass
+    {
+        public static class Nested
+        {
+            public static void Method()
+            {
+                Console.WriteLine("Static Method of Nested Class");
+            }
+        }
+    }
+
+...
+MyClass.Nested.Method();//Static Method of Nested Class
