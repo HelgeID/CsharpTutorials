ac3799b8ece9b3f055c185f1a20c081ad69d4f5f
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-27 13:31:43 +0200

    Add files via upload

---
+++ list8.0.1, вложені класи (наслідування).cs
@@ -0,0 +1,53 @@
+﻿//вложені класи (наслідування)
+
+//приклад1
+    public class Base
+    {
+        public void MethodBase()
+        {
+            Console.WriteLine("Base Method");
+        }
+    }
+
+    class MyClass
+    {
+        public class Nested : Base
+        {
+            public void MethodNested()
+            {
+                Console.WriteLine("Nested Method");
+            }
+        }
+    }
+
+...
+MyClass.Nested ins = new MyClass.Nested();
+ins.MethodBase();//Base Method
+ins.MethodNested();//Nested Method
+
+
+
+//приклад2
+    public class Base
+    {
+        public void MethodBase()
+        {
+            Console.WriteLine("Base Method");
+        }
+    }
+
+    class MyClass : Base
+    {
+        public class Nested //наслідування із Base недопустимо
+        {
+            public void MethodNested()
+            {
+                Console.WriteLine("Nested Method");
+            }
+        }
+    }
+
+...
+MyClass.Nested ins = new MyClass.Nested();
+//ins.MethodBase();//Base Method
+ins.MethodNested();//Nested Method
