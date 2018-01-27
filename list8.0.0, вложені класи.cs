de4b87e23a76068737942034b5b8c5d6c4ea2e0a
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-27 13:30:50 +0200

    Add files via upload

---
+++ list8.0.0, вложені класи.cs
@@ -0,0 +1,58 @@
+﻿//вложені класи
+
+//приклад1
+    class MyClass
+    {
+        public class NestedClass
+        {
+            public void MyMethod()
+            {
+                Console.WriteLine("Nested Method");
+            }
+        }
+    }
+
+...
+MyClass.NestedClass obj = new MyClass.NestedClass();
+obj.MyMethod();//Nested Method
+
+
+
+//приклад2
+    class MyClass
+    {
+        private static int myField = 0;
+        public class NestedClass
+        {
+            public void MyMethod(int _myField)
+            {
+                myField = _myField;
+                Console.WriteLine(myField);
+            }
+        }
+    }
+
+...
+MyClass.NestedClass obj = new MyClass.NestedClass();
+obj.MyMethod(100);//100
+
+
+
+//приклад3
+    class MyClass
+    {
+        private int myField = 0;//поле не статичне
+        public class NestedClass
+        {
+            MyClass ins = new MyClass();//екземпляр класа
+            public void MyMethod(int _myField)
+            {
+                ins.myField = _myField;
+                Console.WriteLine(ins.myField);
+            }
+        }
+    }
+
+...
+MyClass.NestedClass obj = new MyClass.NestedClass();
+obj.MyMethod(100);//100
