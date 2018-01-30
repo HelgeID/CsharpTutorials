a825a3728ae9a4d8d51d28444cee1d7cfd097f36
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-30 23:16:31 +0200

    Add files via upload

---
+++ list9.0.3, властивості у структурах.cs
@@ -0,0 +1,10 @@
+﻿//властивості у структурах
+    struct MyStruct
+    {
+        public int MyProperty { get; set; }
+    }
+
+...
+MyStruct obj = new MyStruct();
+obj.MyProperty = 500;
+Console.WriteLine(obj.MyProperty);//500
