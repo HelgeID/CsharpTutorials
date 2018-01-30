5ae9a4d0749f61975a08fbc7d006d4fddcbbdfd0
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-30 23:17:18 +0200

    Add files via upload

---
+++ list9.0.4, структури (наслідування).cs
@@ -0,0 +1,50 @@
+﻿//структури (наслідування)
+
+//наслідування від інтерфейсів
+    interface MyInterface
+    {
+        void MyMethod();
+    }
+
+    struct MyStruct: MyInterface //(наслідування від класів та структур заборонено)
+    {
+        //реалізація метода
+        public void MyMethod()
+        {
+            Console.WriteLine("My Interface Method");
+        }
+    }
+
+...
+MyStruct ins;
+ins.MyMethod();
+
+
+
+//перевірка наслідування від ValueType
+    struct MyStruct// :ValueType
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+
+...
+MyStruct ins = new MyStruct();
+ValueType vt = ins as ValueType;
+Console.WriteLine(ins.GetHashCode());
+Console.WriteLine(vt.GetHashCode());
+
+
+
+//від структур не можна наслідуватися
+    struct MyStruct
+    {
+        //protected int myField;//error
+    }
+
+    //error
+    //struct MyClass : MyStruct
+    //{
+    //}
