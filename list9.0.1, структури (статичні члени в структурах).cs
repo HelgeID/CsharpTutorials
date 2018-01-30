e426a4ca23464343f4244ed9176e8f5552a108a9
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-30 23:15:26 +0200

    Add files via upload

---
+++ list9.0.1, структури (статичні члени в структурах).cs
@@ -0,0 +1,21 @@
+﻿//структури (статичні члени в структурах)
+
+    struct MyStruct//статична структура не допустима
+    {
+        //статична автовластивість myField
+        public static int MyField
+        {
+            get;
+            set;
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine(MyField);
+        }
+    }
+
+...
+MyStruct.Show();//0
+MyStruct.MyField = 100;
+MyStruct.Show();//100
