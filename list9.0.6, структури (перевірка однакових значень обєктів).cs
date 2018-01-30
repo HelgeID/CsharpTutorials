5f89015e44e2a880a0679b8e01e9c76842719ee0
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-30 23:18:17 +0200

    Add files via upload

---
+++ list9.0.6, структури (перевірка однакових значень обєктів).cs
@@ -0,0 +1,22 @@
+﻿//структури, перевірка однакових значень обєктів
+    public struct MyStruct
+    {
+        public int myNumber;
+        public string myStr;
+
+        public MyStruct(int _myNumber, string _myStr)
+        {
+            myNumber = _myNumber;
+            myStr = _myStr;
+        }
+    }
+
+...
+MyStruct s1 = new MyStruct(100, "Hello!");
+
+MyStruct s2;
+s2.myNumber = 100;
+s2.myStr = "Hello!";
+
+if (s2.Equals(s1))
+    Console.WriteLine("s1 and s2 have same value");
