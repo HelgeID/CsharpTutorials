29f30193fecf46f505c71a2256274038b84acff4
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-30 23:17:48 +0200

    Add files via upload

---
+++ list9.0.5, Nested - структури.cs
@@ -0,0 +1,15 @@
+﻿//Nested - структури
+    struct MyStruct
+    {
+        public struct Nested //також можуть бути класи
+        {
+            public void NestedMethod()
+            {
+                Console.WriteLine("Nested Method");
+            }
+        }
+    }
+
+...
+MyStruct.Nested ins = new MyStruct.Nested();
+ins.NestedMethod();//Nested Method
