50e690d092c784a55d41492028687d56c92fed00
HelgeID <34999595+HelgeID@users.noreply.github.com>
2018-01-31 12:42:09 +0200

    Add files via upload

---
+++ list10.0.0, Перечислення.cs
@@ -0,0 +1,41 @@
+﻿//перечислення
+    struct MyStruct
+    {
+        public const byte zero = 0;
+        public const byte one = 1;
+        public const byte two = 2;
+        public const byte three = 3;
+    }
+
+    enum EnumType : byte //явно вказуємо тип
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+
+...
+Console.WriteLine(EnumType.One);//One
+Console.WriteLine((byte)EnumType.One);//1
+EnumType digit = EnumType.Zero;
+Console.WriteLine(digit);//Zero
+Console.WriteLine((byte)digit);//0
+
+Enum one = EnumType.One;
+Console.WriteLine(one);//One
+EnumType digit = EnumType.Zero;
+Enum zero = digit;
+Console.WriteLine(zero);//Zero
+
+Console.WriteLine(new string('-', 10));
+for (EnumType n = EnumType.Zero; n <= EnumType.Three; n++)
+    Console.WriteLine(n);
+
+/*
+Zero
+One
+Two
+Three
+
+*/
